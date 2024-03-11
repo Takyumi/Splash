4b6253c6c642c37d8c9165e20ee0e5b1b6373c30
Grace Xin <qingqinggrace@gmail.com>
2024-03-11 19:05:10 -0400

    Change cone color

--- shaders/coneFragment.glsl
+++ shaders/coneFragment.glsl
@@ -1,7 +1,7 @@
 varying vec3 vertexNormal;
 
 void main() {
-    float intensity = pow(-dot(vertexNormal, vec3(0.0, 0.0, 1.0)), 2.0) + 0.2;
+    float intensity = pow(-dot(vertexNormal, vec3(0.0, 0.0, 1.0)), 2.0)/1.25;
     //toggle globe intensity
-    gl_FragColor = vec4(0.3, 0.6, 1.0, 1.0) * intensity;
+    gl_FragColor = vec4(0.3, 0.6, 1.0, 1.0) * (1.0 - intensity);
 }
